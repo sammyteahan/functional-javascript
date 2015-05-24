1bd2a760c201e83e85957661e561c554b7395f60
Sammy Teahan <sam.teahan@aggiemail.usu.edu>
2015-05-24 11:13:59 -0600

    Solution for every-some problem

---
+++ five.js
@@ -0,0 +1,30 @@
+/**
+ * Every + Some
+ *
+ * every: checks that all elements pass callback predicate function
+ * some: checks that at least one elements passes callback predicate function
+ */
+
+var goodUsers = [
+  {id: 1},
+  {id: 2},
+  {id: 3},
+  {id: 4},
+  {id: 5}
+];
+
+// Note: we'll have access to array passed into testAllValid within return function 'submittedUsers'
+// example call: var testAllValid = checkUsersValid(goodUsers);
+// testAllValid([{id: 1}, {id: 2}]);
+
+function checkUsersValid(goodUsers) {
+  return function(submittedUsers) {
+    return submittedUsers.every(function(submittedUser) {
+      return goodUsers.some(function(goodUser) {
+        return goodUser.id === submittedUser.id;
+      });
+    });
+  };
+}
+
+module.exports = checkUsersValid;
