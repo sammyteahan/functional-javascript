b97dfb3a004bef3a669e982fd3c36ee023af53d0
Sammy Teahan <sam.teahan@aggiemail.usu.edu>
2015-05-26 07:58:55 -0600

    Boilerplate reduce code

---
+++ six.js
@@ -0,0 +1,14 @@
+/**
+ * Reduce
+ */
+
+// Write a function that takes an array and return a hash-map of how many
+// times each string occurs in the array
+
+function countWords(inputWords) {
+  inputWords.reduce(function(prevWord, currentWord) {
+
+  });
+}
+
+module.exports = countWords;
